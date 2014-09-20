bcc6db48f8f148990cbd84a9b6fd71c43e3a5cf0
Eric Dill <edill@bnl.gov>
2014-09-20 10:22:23 -0400

    MNT: Moar corrections to setup.py for windows issues

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ from setupext import ext_modules
 import numpy as np
 import os
 import shutil
+from subprocess import call
 
 setup(
     name='vttools',
@@ -33,22 +34,31 @@ turns out that you can't check for the presence of symlinks in windows at all.
 http://stackoverflow.com/questions/15258506/os-path-islink-on-windows
 -with-python
 """
+from sys import platform as _platform
 
 if os.path.islink(dst):
     # unlink it
     os.unlink(dst)
+    print('unlinked: {0}'.format(dst))
+elif _platform == 'win32':
+    # you're on windows and os.path.islink alwasys reports False in py2.7
+    # http://stackoverflow.com/questions/15258506/os-path-islink-on-windows-with-python
+    # assume that this is a symbolic link
+    call(['rmdir', dst], shell=True)
 elif os.path.isdir(dst):
     # remove it
     try:
         shutil.rmtree(dst)
+        print("rmtree'd: {0}".format(dst))
     except WindowsError as whee:
         # you're on windows and os.path.islink alwasys reports False in py2.7
         # http://stackoverflow.com/questions/15258506/os-path-islink-on-windows-with-python
-        # treat it as a file and remove it
-        os.path.remove(dst)
+        call(['rmdir', dst], shell=True)
+        print("rmdir'd: {0}".format(dst))
 elif os.path.isfile(dst):
     # remove it
     os.remove(dst)
+    print("os.remove'd: {0}".format(dst))
 else:
     # the file probably doesn't exist
     pass
