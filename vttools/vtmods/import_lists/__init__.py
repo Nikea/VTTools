3c6c0daff695c53747eda77dd2a5b483dc40e19d
Eric Dill <edill@bnl.gov>
2014-09-20 10:09:34 -0400

    MNT: import updated

--- vttools/vtmods/import_lists/__init__.py
+++ vttools/vtmods/import_lists/__init__.py
@@ -1,1 +1,67 @@
-__author__ = 'edill'
+# ######################################################################
+# Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
+# National Laboratory. All rights reserved.                            #
+#                                                                      #
+# Redistribution and use in source and binary forms, with or without   #
+# modification, are permitted provided that the following conditions   #
+# are met:                                                             #
+#                                                                      #
+# * Redistributions of source code must retain the above copyright     #
+#   notice, this list of conditions and the following disclaimer.      #
+#                                                                      #
+# * Redistributions in binary form must reproduce the above copyright  #
+#   notice this list of conditions and the following disclaimer in     #
+#   the documentation and/or other materials provided with the         #
+#   distribution.                                                      #
+#                                                                      #
+# * Neither the name of the Brookhaven Science Associates, Brookhaven  #
+#   National Laboratory nor the names of its contributors may be used  #
+#   to endorse or promote products derived from this software without  #
+#   specific prior written permission.                                 #
+#                                                                      #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       #
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,           #
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   #
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR   #
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   #
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  #
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OTHERWISE) ARISING   #
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                          #
+########################################################################
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import six
+import os
+import yaml
+import logging
+logger = logging.getLogger(__name__)
+
+
+def load_config(yaml_file=None):
+    """Read and return yaml_file
+
+    If yaml_file is not provided, this function reads the 'modules.yaml'
+    file in this directory
+
+    Parameters
+    ----------
+    yaml_file : str, optional
+        Import configuration file to read in and return
+
+    Returns
+    -------
+    import_dict : dict
+        Dictionary describing the functions/classes/Modules that should be
+        available in VisTrails
+    """
+    # read yaml modules
+    with open(os.path.join((os.path.dirname(os.path.realpath(__file__))),
+                           'modules.yaml'), 'r') as modules:
+        import_dict = yaml.load(modules)
+        print('import_dict: {0}'.format(import_dict))
+        return import_dict
