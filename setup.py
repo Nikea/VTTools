a7ef06feab9a99c4a1479ce96e5e38ad8501c62e
Eric Dill <edill@bnl.gov>
2014-09-20 09:55:44 -0400

    MNT: Refactored the userpackages directory creation
    - New approach makes a symlink from /VTTools/vt_config/NSLS-II
      to ~/.vistrails/userpackages/NSLS-II
    - This is tested on windows, needs testing on linux and mac
    - NOTE: The script blows away anything currently in
      ~/.vistrails/userpackages/NSLS-II

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import setuptools
 from distutils.core import setup, Extension
 from setupext import ext_modules
 import numpy as np
+import os
+import shutil
 
 setup(
     name='vttools',
@@ -15,3 +17,51 @@ setup(
     include_dirs=[np.get_include()],
     ext_modules=ext_modules
     )
+
+# this_file_loc = os.path.realpath(__file__)
+src = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'vt_config',
+                   'NSLS-II')
+dst = os.path.join(os.path.expanduser('~'), '.vistrails', 'userpackages',
+                   'NSLS-II')
+
+print('src: {0}'.format(src))
+print('dst: {0}'.format(dst))
+# todo check for the presence of ~/.vistrails/userpackages
+# clear out any existing stuff in the userpackages/NSLS-II folder
+"""
+turns out that you can't check for the presence of symlinks in windows at all.
+http://stackoverflow.com/questions/15258506/os-path-islink-on-windows
+-with-python
+"""
+
+if os.path.islink(dst):
+    # unlink it
+    os.unlink(dst)
+elif os.path.isdir(dst):
+    # remove it
+    try:
+        shutil.rmtree(dst)
+    except WindowsError as whee:
+        # you're on windows and os.path.islink alwasys reports False in py2.7
+        # http://stackoverflow.com/questions/15258506/os-path-islink-on-windows-with-python
+        # treat it as a file and remove it
+        os.path.remove(dst)
+elif os.path.isfile(dst):
+    # remove it
+    os.remove(dst)
+else:
+    # the file probably doesn't exist
+    pass
+
+# this symlink does not get removed when pip uninstall vttools is run...
+# todo figure out how to make pip uninstall remove this symlink
+try:
+    # symlink the NSLS-II folder into userpackages
+    os.symlink(src, dst)
+except AttributeError as ae:
+    # you must be on Windows!
+    from subprocess import call
+    call(['mklink', '/j', dst, src], shell=True)
+
+# shutil.copytree(src, dst, symlinks=True)
+# osos.path.expanduser('~/')
---
+++ vt_config/NSLS-II/init.py
@@ -0,0 +1,104 @@
+# ######################################################################
+# Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
+# National Laboratory. All rights reserved.                            #
+#                                                                      #
+# Redistribution and use in source and binary forms, with or without   #
+# modification, are permitted provided that the following conditions   #
+# are met:                                                             #
+#                                                                      #
+# * Redistributions of source code must retain the above copyright     #
+#   notice, this list of conditions and the following disclaimer.      #
+#                                                                      #
+# * Redistributions in binary form must reproduce the above copyright  #
+#   notice this list of conditions and the following disclaimer in     #
+#   the documentation and/or other materials provided with the         #
+#   distribution.                                                      #
+#                                                                      #
+# * Neither the name of the Brookhaven Science Associates, Brookhaven  #
+#   National Laboratory nor the names of its contributors may be used  #
+#   to endorse or promote products derived from this software without  #
+#   specific prior written permission.                                 #
+#                                                                      #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       #
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,           #
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   #
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR   #
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   #
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  #
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OTHERWISE) ARISING   #
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                          #
+########################################################################
+'''
+Created on Apr 29, 2014
+'''
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import six
+import logging
+import sys
+import yaml
+import importlib
+import collections
+import os
+from vttools import wrap_lib, load_config
+from vttools.wrap_lib import AutowrapError
+logger = logging.getLogger(__name__)
+
+asd
+# read yaml modules
+with open(os.path.join((os.path.dirname(os.path.realpath(__file__))),
+                       'modules.yaml'), 'r') as modules:
+    import_dict = yaml.load(modules)
+    print('import_dict: {0}'.format(import_dict))
+
+import_dict = vttools.load_config()
+
+def get_modules():
+    # set defaults
+    try:
+        # import the hand-built VisTrails modules
+        module_list = import_dict['import_modules']
+        pymods = [importlib.import_module(module_name, module_path)
+                  for module_path, mod_lst in six.iteritems(module_list)
+                  for module_name in mod_lst]
+        # autowrap functions
+        func_list = import_dict['autowrap_func']
+        vtfuncs = [wrap_lib.wrap_function(**func_dict)
+                   for func_dict in func_list]
+        # autowrap classes
+        # class_list = import_dict['autowrap_classes']
+        # vtclasses = [wrap_lib.wrap_function(**func_dict)
+        #              for func_dict in class_list]
+    except ImportError as ie:
+        msg = ('importing {0} failed\nOriginal Error: {1}'
+               ''.format(module_name, module_path, ie))
+        print(msg)
+        logging.error(msg)
+        six.reraise(*sys.exc_info())
+    except AutowrapError as ae:
+        msg = ('autowrapping {0} failed\nOriginal Error: {1}'
+               ''.format(func_dict, ae))
+        print(msg)
+        logging.error(msg)
+        six.reraise(*sys.exc_info())
+
+    vtmods = [vtmod for mod in pymods for vtmod in mod.vistrails_modules()]
+
+    all_mods = vtmods + vtfuncs  # + vtclasses
+    if len(all_mods) != len(set(all_mods)):
+        raise ValueError('Some modules have been imported multiple times.\n'
+                         'Full list: {0}'
+                         ''.format([x for x, y in
+                                    collections.Counter(all_mods).items()
+                                    if y > 1]))
+
+    # return the valid VisTrails modules
+    return all_mods
+
+
+# # init the modules list
+_modules = get_modules()
