97d5d3532466f458bdec7153efa8802502a71ccb
Gabriel Iltis <giltis@bnl.gov>
2014-10-20 19:58:23 -0400

    DEV: Updated wrap_lib RE defs to match enum_test branch defs and details

--- vttools/wrap_lib.py
+++ vttools/wrap_lib.py
@@ -262,15 +262,14 @@ def _type_optional(type_str):
 
 
 _ENUM_RE = re.compile('\{(.*)\}')
-
-
-# TODO: We may want to make more thorough use of this RE by using the
-# specified values inside the parentheses as an additional input or output
-# port key (e.g. 2D, 3D, 1xN, NxN, NxM, LxMxN, etc.)
 _ARRAY_SHAPE = re.compile('\(([A-Za-z0-9]+,? *)+,?\) *array')
-# RE Details:
-# WORKS: test_str5 = "(your, mother, was, a, hampster) array"
-# WORKS: test_str5 = "(your, mother, was, a, hampster,) array"
+#   RE Details:
+#   WORKS: test_str5 = "(your, mother, was, a, hampster) array"
+#   WORKS: test_str5 = "(your, mother, was, a, hampster,) array"
+# TODO: We may want to make more thorough use of this RE by using the
+#   specified values inside the parentheses as an additional input or output
+#   port key (e.g. 2D, 3D, 1xN, NxN, NxM, LxMxN, etc.)
+
 
 def _enum_type(type_str):
     """
