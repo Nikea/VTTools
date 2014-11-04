33cf528d340bd3e9aa99e786b85d92d8257a593f
Eric Dill <edill@bnl.gov>
2014-11-04 08:47:27 -0500

    ENH: Added ModelAggregator VisTrails module
    - Also updated yaml file based on my changes to NSLS2.fitting

---
+++ vttools/vtmods/fitting.py
@@ -0,0 +1,69 @@
+# ######################################################################
+# Copyright (c) 2014, Brookhaven Science Associates, Brookhaven        #
+# National Laboratory. All rights reserved.                            #
+#                                                                      #
+# Redistribution and use in source and binary forms, with or without   #
+# modification, are permitted provided that the following conditions   #
+# are met:                                                             #
+#                                                                      #
+# * Redistributions of source code must retain the above copyright     #
+#   notice, this list of conditions and the following disclaimer.      #
+#                                                                      #
+# * Redistributions in binary form must reproduce the above copyright  #
+#   notice this list of conditions and the following disclaimer in     #
+#   the documentation and/or other materials provided with the         #
+#   distribution.                                                      #
+#                                                                      #
+# * Neither the name of the Brookhaven Science Associates, Brookhaven  #
+#   National Laboratory nor the names of its contributors may be used  #
+#   to endorse or promote products derived from this software without  #
+#   specific prior written permission.                                 #
+#                                                                      #
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS  #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT    #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS    #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE       #
+# COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,           #
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES   #
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR   #
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)   #
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,  #
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OTHERWISE) ARISING   #
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE   #
+# POSSIBILITY OF SUCH DAMAGE.                                          #
+########################################################################
+
+from __future__ import (absolute_import, division, print_function,
+                        unicode_literals)
+import six
+from vistrails.core.modules.vistrails_module import Module, ModuleSettings
+from vistrails.core.modules.config import IPort, OPort
+import logging
+logger = logging.getLogger(__name__)
+
+class ModelAggregator(Module):
+    """Combine 1+ models into an aggregate model
+    """
+    _settings = ModuleSettings(namespace='fitting')
+    _input_ports = [
+        IPort(name='models', label='models to aggregate',
+              signature='basic:Variant')
+    ]
+    _output_ports = [
+        OPort(name='aggregated_models', signature='basic:Variant')
+    ]
+    def compute(self):
+        """Mandatory override of parent `Module` class.
+
+        Loop over however many models are connected to the 'models' input port
+        and combine them into a single aggregate model
+        """
+        models = self.get_input_list('models')
+        aggregated = models[0]
+        if len(models) > 2:
+            for model in models[1:]:
+                aggregated += model
+        self.set_output('aggregated_models', aggregated)
+
+def vistrails_modules():
+    return [ModelAggregator,]
