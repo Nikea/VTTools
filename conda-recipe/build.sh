eb5a28e6f5cd6306bab8da8dd9b5e214bd44eab7
Thomas A Caswell <tcaswell@bnl.gov>
2015-01-21 14:57:20 -0500

    BLD : fix install path for packages

--- conda-recipe/build.sh
+++ conda-recipe/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 $PYTHON setup.py install
-cp -r vt_config/NSLS-II $SP_DIR/vistrails/packages/
+DIR=`$PYTHON -c 'from __future__ import print_function;import vistrails.core.system; print(vistrails.core.system.packages_directory())'`
+cp -r vt_config/NSLS-II $DIR
